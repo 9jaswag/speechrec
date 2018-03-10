96dafc6cd08432043a32c05d70f6e987824a7a6c
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-03-10 17:07:23 +0100

    add mic icon and chime sound

--- main.js
+++ main.js
@@ -1,58 +1,91 @@
-window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition; // available in firefox and chrome and lives on top of the window object
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
-recognition.interimResults = true; // Controls whether interim results should be returned (true) or not (false.)
-//Interim results are results that are not yet final. popualates the speech as you speak
-// has to be run via a server
+// recognition.continuous = true;
+recognition.interimResults = true;
 const synth = window.speechSynthesis;
 
-let p = document.createElement('p'); // we create a paragraph
-let container = document.querySelector('.container'); // select our container
-container.appendChild(p); // append it
-// console.log(container);
 
-recognition.addEventListener('result', e => { // add an eventlistener to the recognition
-  const speechToText = Array.from(e.results) // list of result..not an array
+const icon = document.querySelector('i.fa.fa-microphone')
+let paragraph = document.createElement('p');
+let container = document.querySelector('.text-box');
+const sound = document.querySelector('.sound');
+container.appendChild(paragraph);
+let listening = false;
+
+recognition.onstart = function() {
+  listening = true;
+  console.log('Speech recognition service has started');
+};
+
+recognition.onend = function() {
+  console.log('Speech recognition service disconnected');
+};
+
+const dictate = () => {
+  console.log('dictating');
+  recognition.onresult = (event) => {
+    const speechToText = Array.from(event.results)
     .map(result => result[0])
     .map(result => result.transcript)
-    .join('') // join the peices in the array
-  // console.log(speechToText) // something else must not be using your mic
+    .join('');
     
-    p.textContent = speechToText;
+    paragraph.textContent = speechToText;
 
-    if (e.results[0].isFinal) { // last - check if result is final
-      p = document.createElement('p'); // create a new paragraph so it doesn't clear our previous text
-      container.appendChild(p);
-      // window.scrollTo(0, container.scrollHeight);
-
-      if (speechToText.includes('ok google')) {
-        voices = synth.getVoices();
-        console.log(voices[32])
-        utterThis = new SpeechSynthesisUtterance('how can I help you?');
-        utterThis.voice == voices[32]
-        synth.speak(utterThis);
-      }
-
-      if (speechToText.includes('what is today\'s date')) {
-        voices = synth.getVoices();
-        const time = new Date(Date.now())
-        utterThis = new SpeechSynthesisUtterance(time.toLocaleDateString());
-        utterThis.voice == voices[32]
-        synth.speak(utterThis);
-      }
+    if (event.results[0].isFinal) {
+      container.scrollTo(0, container.scrollHeight);
+      paragraph = document.createElement('p');
+      container.appendChild(paragraph);
 
       if (speechToText.includes('what is the time')) {
-        voices = synth.getVoices();
-        const time = new Date(Date.now())
-        utterThis = new SpeechSynthesisUtterance(
-          time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
-        );
-        utterThis.voice == voices[32]
-        synth.speak(utterThis);
-      }
+        speak(getTime);
+      };
+
+      if (speechToText.includes('what is today\'s date')) {
+        speak(getDate);
+      };
+
+      if (speechToText.includes('what is the weather in')) {
+        // speak(getTheWeather)
+        getTheWeather(speechToText);
+      };
     }
+  };
+
+  recognition.onend = recognition.start
+  recognition.start();
+};
+
+const getTime = () => {
+  const time = new Date(Date.now());
+  return `the time is ${time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`
+};
+
+const getDate = () => {
+  const time = new Date(Date.now())
+  return `today is ${time.toLocaleDateString()}`;
+};
+
+const getTheWeather = (speech) => {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${speech.split(' ')[5]}&appid=6aa90859f3e957ff6c77ec9b1bc86296&units=metric`)
+  .then(function(response){
+    return response.json();
+  }).then(function(weather){
+    utterThis = new SpeechSynthesisUtterance(`the weather condition in ${weather.name} is mostly full of
+    ${weather.weather[0].description} at a temperature of ${weather.main.temp} degrees Celcius`);
+    synth.speak(utterThis);
+  });
+};
+
+const speak = (action) => {
+  utterThis = new SpeechSynthesisUtterance(action());
+  synth.speak(utterThis);
+};
+
+icon.addEventListener('click', () => {
+  if (listening) {
+    recognition.stop();
+    return;
+  }
+  sound.play();
+  dictate();
 });
-
-recognition.addEventListener('end', recognition.start); //  sec to last -another event listener to restart the recognition when it ends
-recognition.start(); // start the recognition
-
-// learn the basics of building speech to text apps in 10 mins
